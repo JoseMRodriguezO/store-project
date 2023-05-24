3cec585e61c08ed4f165b03726340ce7b2b0b233
JoseMRodriguez <Josemiguelrodriguez89@gmail.com>
2023-05-24 09:52:13 -0500

    Signup and login forms

--- src/Content.jsx
+++ src/Content.jsx
@@ -1,7 +1,12 @@
+import { Signup } from "./Signup";
+import { Login } from "./Login";
+
 export function Content() {
   return (
     <div>
       <h1>Welcome to React!</h1>
+      {<Signup />}
+      {<Login />}
     </div>
   );
 }
---
+++ src/ProductIndex.jsx
@@ -0,0 +1,7 @@
+export function ProductIndex() {
+  return (
+    <div>
+      <h1>All products</h1>
+    </div>
+  );
+}
